1fea8af9b764b87212ec49f61c8df243c55451aa
Greg <gregcourt10@gmail.com>
2023-03-20 12:36:46 +0000

    changed image from object-fit to object-cover

--- src/containers/ImageContainer.jsx
+++ src/containers/ImageContainer.jsx
@@ -7,7 +7,7 @@ export const ImageContainer = () => {
 
   return (
     <div className="imageContainer h-1/2 flex justify-center">
-      <img src={lobby} alt="Lobby" className="h-full object-contain"/>
+      <img src={lobby} alt="Lobby" className="h-full object-cover"/>
     </div>
   );
 };
