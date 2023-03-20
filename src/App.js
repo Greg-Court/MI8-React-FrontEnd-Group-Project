fb3775fa12d03456529826b71c98afee5b31ee72
Greg <gregcourt10@gmail.com>
2023-03-20 14:10:11 +0000

    added appinitialiser.js - NOT YET TESTED

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import { ChatContainer } from "./containers/ChatContainer";
 import { ImageContainer } from "./containers/ImageContainer";
 import { InventoryContainer } from "./containers/InventoryContainer";
+import { initialiseApp } from "./AppInitialiser";
 
 function App() {
+
+  useEffect(() => {
+    initialiseApp('player1', '1');
+  }, []);
+
   return (
     <div className="h-screen">
       <ImageContainer/>
--- src/containers/ChatContainer.jsx
+++ src/containers/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { ChatBubbleRecieve } from '../components/ChatBubbleRecieve'
 import { ChatBubbleSend } from '../components/ChatBubbleSend'
 
@@ -6,13 +6,10 @@ import { ChatBubbleSend } from '../components/ChatBubbleSend'
 export const ChatContainer = () => {
     const [messages, setMessages] = useState()
 
-
-
     return (
     <div className="bg-blue-500 w-1/2 p-3">
         <ChatBubbleSend/>
         <ChatBubbleRecieve/>
-        <p>test</p>
     </div>
   )
 }
