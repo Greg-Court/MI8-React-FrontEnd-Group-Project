37c7620ff1077cb97dfb54c3abedfb0754c9bbb5
Hansine Marshall <hansinemarshall@Hansines-MacBook-Pro.local>
2023-03-20 15:24:23 +0000

    added inventory list

--- src/components/InventoryItem.jsx
+++ src/components/InventoryItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-export const InventoryItem = ({item, itemIcon}) => {
+
+export const InventoryItem = ({item, itemIcon, text}) => {
   return (
   <div className='flex items-center'>
     <div className="p-3 max-w-xl mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 mt-2 justify-start">
       <div className="shrink-0">
         <img
           src={itemIcon}
-          alt="ChitChat Logo"
+          alt= "photo"
           className="h-12 w-12"
         />
       </div>
